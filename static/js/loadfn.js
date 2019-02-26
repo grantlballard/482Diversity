b124fd1443a76637f6c390c1339c4b2b3e8b84b0
runner123g <runner123g@tamu.edu>
2019-02-26 16:25:31 -0600

    Added load functionality, Displays csv contents in console currently

--- static/js/loadfn.js
+++ static/js/loadfn.js
@@ -1,17 +1,31 @@
+/*
+    Could have made this a 
+    on click fn but oh well
+    just clicks the input which 
+    brings up the user's file browser
+*/
 function loadcsv() {
     $("#files").click();
-    $(() => {
-        $("#files").change(loadCodeCallback);
-    });
+    //$("#files").change();
 }
 
-/**
- * 
- * @param {event Handler} e 
- */
+
+/*
+    Bind handlesel function to file change
+    Whenever user chooses  file it 
+
+*/
+$(document).on("change" ,"#files" ,function(){handlesel();});
 
 
-function loadCodeCallback(e) {
+/*
+    function to handle the file the user uploaded
+    reads in the file's text content, file needs to be a 
+    csv or else the user will get an alert.
+    Returns True on success, False otherwise
+*/
+function handlesel() {
+    console.log("In changefn");
     var files = document.getElementById('files').files;
 
     if (!files.length) {
@@ -20,45 +34,43 @@ function loadCodeCallback(e) {
         return false;
     }
 
-    //if (loadVerbose) {
-        for (var i = 0; i < files.length; i++) {
-            console.log("Filename: " + files[i].name);
-            console.log("Type: " + files[i].type);
-            console.log("Size: " + files[i].size + " bytes");
-        }
-    //}
+    for (var i = 0; i < files.length; i++) {
+        console.log("Filename: " + files[i].name);
+        console.log("Type: " + files[i].type);
+        console.log("Size: " + files[i].size + " bytes");
+    }
+    
 
     // Validate Extensions
-    // If file isn't a kjs or js file, then alert and quit. 
+    // If file isn't a csv file, then alert and quit. 
     var allowedExtensions = /(\.csv)$/i;
 
     if (!allowedExtensions.exec(files[0].name)) {
-        alert('Please upload file having extensions .js/.kjs only.');
+        alert('Please upload .csv only.');
         return false;
     }
-
-    var file = files[0];
+    // Determine start and stop positions for file
+    var file = files[0];    
     var start = 0;
     var stop = file.size - 1;
 
+    //check if file is empty
     if (stop == -1) {
         alert("Error: The file is empty.");
         return false;
     }
 
     var reader = new FileReader();
-
+    // Once done reading in flle we can edit the text
     reader.onloadend = function (evt) {
         if (evt.target.readyState == FileReader.DONE) {
-
-            codeToReload = evt.target.result;
-           // $('#code-element').text(codeToReload);
-            // Cookies.set('code', codeToReload);
-            //globalObject.codeElement.setValue(codeToReload);
-           
+            response_text = evt.target.result;
+            //console.log("here");
+            console.log(response_text);
         }
     };
 
+    // Read in file as string
     var blob = file.slice(start, stop + 1);
     reader.readAsBinaryString(blob);
     return true;
