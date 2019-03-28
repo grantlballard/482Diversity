5340262cfc0c29ec0b38dc0fcb8b6cd28a3480c0
Julian <juliana42@tamu.edu>
2019-03-28 16:38:52 -0500

    Moved around project structure and added React Table Sample on results page

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder="./static/js")
+
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def api_home():
+	return render_template("home.html")
 
 @app.route("/results")
 def results():
     return render_template("results.html")
 
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
---
+++ static/js/index.jsx
@@ -0,0 +1,8 @@
+// index.jsx
+import React from "react";
+import ReactDOM from "react-dom";
+import Table from "./Table";
+
+import "react-table/react-table.css";
+
+ReactDOM.render(<Table />, document.getElementById("table-content"));
